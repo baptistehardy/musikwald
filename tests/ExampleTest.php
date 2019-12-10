5c160379660053fc8d08abfe0033fe102e47d44a
Baptiste HARDY <baptistehardy.dev@gmail.com>
2019-12-10 14:06:04 +0100

    :white_check_mark: Add ExampleTest

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_returnTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
