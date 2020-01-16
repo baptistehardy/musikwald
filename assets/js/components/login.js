bb1fea96a3488fe6488db02ebf46d75284f6a823
Baptiste HARDY <baptistehardy.dev@gmail.com>
2020-01-16 10:57:49 +0100

    Add login component

---
+++ assets/js/components/login.js
@@ -0,0 +1,46 @@
+import * as React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authenticated: false
+        }
+    }
+
+    isAuthenticated() {
+        fetch(`http://127.0.0.1:8000/api/authenticated`, {
+            method: 'GET',
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                json = JSON.parse(json);
+
+                this.setState({
+                    authenticated: json.authenticated
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.isAuthenticated()
+    }
+
+    render() {
+
+        if (this.state.authenticated) {
+            return (<br/>)
+        } else {
+            return (
+                <ListItem button component="a" href="/login">
+                    <ListItemText primary="Login" />
+                </ListItem>
+            )
+        }
+    }
+}
