28f07c3bd87d1e1f0d717536e4fa4c41f9e088a4
Francoisdsr <francois.dsr@outlook.fr>
2020-01-28 11:19:54 +0100

    Ajout des pages listes genres et labels

--- assets/js/components/genreList.js
+++ assets/js/components/genreList.js
@@ -1,9 +1,73 @@
 import * as React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import ArtistListCard from "./artistListCard";
+import {Typography} from "@material-ui/core";
 
 export class GenreList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            genres: {},
+            isLoading: true,
+            authenticated: false
+        }
+    }
+
+    isAuthenticated() {
+        fetch(`http://127.0.0.1:8000/api/authenticated`, {
+            method: 'GET',
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                json = JSON.parse(json);
+
+                this.setState({
+                    authenticated: json.authenticated
+                })
+            });
+    }
+
+    getGenresList() {
+        fetch(`http://127.0.0.1:8000/api/genres`, {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json'
+            }
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                this.setState({
+                    genres: json,
+                    isLoading: false
+                })
+            });
+    }
+
+    componentDidMount() {
+        this.getGenresList();
+        this.isAuthenticated();
+    }
+
+
     render() {
-        return (
-            <h1>Genres</h1>
-        )
+        if (this.state.isLoading) {
+            return <CircularProgress/>
+        } else {
+            this.genres = this.state.genres.map((genre) =>
+                <ArtistListCard key={genre.id} name={genre.label} />
+            );
+
+            return (
+                <div>
+                    <h1>Genres</h1>
+                    <div>{this.genres}</div>
+                </div>
+            )
+        }
     }
 }
