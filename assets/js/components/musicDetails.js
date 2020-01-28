61d1ac9397eccdc93f3e6b528fadd65eeee45748
Francoisdsr <francois.dsr@outlook.fr>
2020-01-28 16:56:33 +0100

    Page d'affichage d'un seul morceau

--- assets/js/components/musicDetails.js
+++ assets/js/components/musicDetails.js
@@ -2,6 +2,14 @@ import * as React from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import {MusicListCard} from "./musicListCard";
 import {Typography} from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import ArtistDisplay from "./artistDisplay";
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+
 
 export class MusicDetails extends React.Component {
     constructor(props) {
@@ -36,12 +44,62 @@ export class MusicDetails extends React.Component {
     componentDidMount() {
         this.getMusic();
     }
-
-
+    
     render() {
-        console.log(this.state.music);
-        return (
-            <Typography>{this.state.music.name}</Typography>
-        )
+        if (this.state.isLoading) {
+            return <CircularProgress/>
+        } 
+        else {
+            this.state.music.duration = Math.trunc(this.state.music.duration/60)+":"+this.state.music.duration%60;
+            console.log(this.state.music);
+            this.artists = "";  
+            this.state.music.artists.map((artist, i) => {
+                if (this.state.music.artists.length === i + 1) {
+                    // last one
+                    this.artists += artist.name;
+                } else {
+                    // not last one
+                    this.artists += artist.name + ", " ;
+                }
+            });
+            return (
+                <div>
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <img src={this.state.music.image} alt={this.state.music.name} width="200" height="200"/>
+                        </Grid>
+                        <Grid item xs={12} sm container>
+                            <Grid item xs container direction="column" spacing={1}>
+                                <Grid item xs>
+                                    <Typography><b>{this.state.music.name}</b></Typography>
+                                    <Typography>Artiste(s): {this.artists}</Typography>
+                                    <Typography>Durée: {this.state.music.duration}</Typography>
+                                    <Typography>ALbum: {this.state.music.album.name}</Typography>
+                                    <Typography>Label: {this.state.music.label.label}</Typography>
+                                </Grid>
+                                <Grid item xs>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        startIcon={<DeleteIcon />}
+                                    >
+                                        Supprimer
+                                    </Button>
+                                    &nbsp;
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        startIcon={<EditIcon />}
+                                    >
+                                        Modifier
+                                    </Button>
+                                
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+            )
+        }
     }
 }
